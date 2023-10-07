baa928d57ab55f7ea3a9cd22ea7b65845c44ca72
Sebastian Rodriguez <r.sebastian2020@correo.unimet.edu.ve>
2023-10-07 00:36:12 -0400

    1.0.10

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -3,21 +3,16 @@
 // * To change this template file, choose Tools | Templates
 // * and open the template in the editor.
 // */
-//package classes;
-//
-///**
-// *
-// * @author juand
-// */
-//public class Main {
-//    public static void main(String[] args){
-//        //se inicializan los estudios
-//    }
-//            
-//}
+package classes;
 
+/**
+ *
+ * @author Sebastian Rodriguez
+ */
 import classes.DesarrolladorNarrativa;
 import java.util.concurrent.Semaphore;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -26,20 +21,32 @@ public class Main {
         int capacidadDrive = 25;
         Semaphore driveN = new Semaphore(capacidadDrive);
 
-        int numeroDeEmpleados = 5; 
+        // Se crea una lista para mantener un seguimiento del numero de hilos de desarrolladores que hay activos
+        List<DesarrolladorNarrativa> desarrolladoresNarrativaX = new ArrayList<>();
 
-        for (int i = 1; i <= numeroDeEmpleados; i++) {
-            String nombreHilo = "empleadoN" + i;
-            DesarrolladorNarrativa empleadoN = new DesarrolladorNarrativa(0, 0, 0, 10, driveN, 0, true, 6);
-            empleadoN.setName(nombreHilo);
-            empleadoN.start();
+        int valorSpinner = 3;
+        int cantidadActual = desarrolladoresNarrativaX.size();
+
+        if (valorSpinner > cantidadActual) {
+            // Se agregan nuevos hilos si se aumenta el valor del spinner
+            for (int i = 0; i < valorSpinner - cantidadActual; i++) {
+                String nombreHilo = "empleadoNarrativaX" + i;
+                DesarrolladorNarrativa empleadoNarrativaX = new DesarrolladorNarrativa(driveN, 0, true, 4);
+                // Se setea un nombre al hilo creado.
+                empleadoNarrativaX.setName(nombreHilo);
+                empleadoNarrativaX.start();
+                // Se agrega a la lista de desarrolladoresNarrativaX el nuevo empleado.
+                desarrolladoresNarrativaX.add(empleadoNarrativaX);
+                System.out.println(desarrolladoresNarrativaX);
+                System.out.println(desarrolladoresNarrativaX);
+            }
+        } else if (valorSpinner < cantidadActual) {
+            for (int i = cantidadActual - 1; i >= valorSpinner; i--) {
+                // aqui podemos buscar el ultimo hilo creado para eliminar, se debe estructurar bien
+                // la logica del .get de la lista para saber cual queremos borrar. 
+                DesarrolladorNarrativa empleadoNarrativaX = desarrolladoresNarrativaX.get(i);
+                // Aqui se elimina el ultimo hilo que se creo, agregar esa logica. 
+            }
         }
-
     }
 }
-
-        
-        // FUNCIONAAA SUUU
-//        }
-    
-
